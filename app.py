fbec48936f1b6bffa5fc6f10f286365414c23532
Souradip pal <105942415+SparklinStar@users.noreply.github.com>
2023-07-03 02:12:48 +0530

    added files

---
+++ app.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import helper
+import preprocessor
+import matplotlib.pyplot as plt
+import seaborn as sns
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data=bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+    st.dataframe(df)
+
+    #fetching users unique
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"overall")
+
+    selected_user = st.sidebar.selectbox("Show Analysis wrt",user_list)
+
+    if st.sidebar.button("Show Analysis"):
+        num_messages,words,media,poll,link = helper.fetch_stats(selected_user,df)
+
+        col1,col2,col3,col4,col5 = st.columns(5)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total words")
+            st.title(words)
+        with col3:
+            st.header("Total media")
+            st.title(media)
+        with col4:
+            st.header("Total Polls")
+            st.title(poll)
+        with col5:
+            st.header("Total Links")
+            st.title(link)
+
+        # monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'])
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+
+        # daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+
+
+        #wordcloud
+        st.title("WorldCloud")
+        df_wc = helper.create_worldcloud(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+
+
+
+
+        col1,col2 =st.columns(2)
+        # most common words
+        with col1:
+            st.header("Word counter")
+            most_common_df=helper.most_common_words(selected_user,df)
+            st.dataframe(most_common_df)
+
+        #emoji counter
+        with col2:
+            st.header("Emoji counter")
+            emoji_df = helper.emoji_helper(selected_user,df)
+            st.dataframe(emoji_df)
---
+++ preprocessor.py
@@ -0,0 +1,37 @@
+import re
+import pandas as pd
+
+
+def preprocess(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[ap]m\s-\s'
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p - ')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+    users = []
+    messages = []
+    for data in df['user_message']:
+        user_message = data.split(':', 1)
+        if len(user_message) == 2:
+            user = user_message[0]
+            message = user_message[1].strip()
+            users.append(user)
+            messages.append(message)
+        else:
+            users.append('group_notification')
+            messages.append(data)
+
+    # Create a DataFrame from the extracted data
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    df['only_date'] = df['date'].dt.date
+
+    return df
