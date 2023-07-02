6b2c774362747f6648dcdc4d4582a4e27a05b4cd
Souradip pal <105942415+SparklinStar@users.noreply.github.com>
2023-07-03 04:14:48 +0530

    12 hour or 24 hour format will change according to device

--- preprocessor.py
+++ preprocessor.py
@@ -4,11 +4,26 @@ import pandas as pd
 
 def preprocess(data):
     pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[ap]m\s-\s'
-    messages = re.split(pattern, data)[1:]
-    dates = re.findall(pattern, data)
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p - ')
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+    pattern2= '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    match_12_hour = re.search(pattern, data)
+    if match_12_hour:
+        messages = re.split(pattern, data)[1:]
+        dates = re.findall(pattern, data)
+        df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p - ')
+        df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    match_24_hour = re.search(pattern2, data)
+    if match_24_hour:
+        messages = re.split(pattern2, data)[1:]
+        dates = re.findall(pattern2, data)
+
+        df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+        # convert message_date type
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+
+        df.rename(columns={'message_date': 'date'}, inplace=True)
+
     users = []
     messages = []
     for data in df['user_message']:
